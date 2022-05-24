1f32a717f8fd42d39194400e629279baae275cfd
花开半亩地 <2572468699@qq.com>
2022-05-25 00:29:18 +0800

    feat: 添加新行,记录输入值

--- src/App.vue
+++ src/App.vue
@@ -4,39 +4,48 @@
 	const rows = ref(
 		Array.apply(null, { length: tableSize[0] }).map((_, i) =>
 			Array.apply(null, { length: tableSize[1] }).map((_, j) => {
-				return { text: i * tableSize[1] + j + 1, class: [] }
+				return { text: '', class: [] }
 			})
 		)
 	)
 
-	function onKeyupHandler(row, y, x, e) {
-		console.log(row, x, y, e)
+	function onKeyupHandler(y, x, e) {
 		const text = e.target.innerText
-		// data.value.rows[y][x] = text
-		// 删除
-		// if (e.key === 'Backspace' && !text.length) {
-		// 	// data.value.rows[y].splice(x, 1)
-		// }
+		rows.value[y][x].text = text
 	}
-	function addRows(y) {
-		// data.value.rows.splice(y + 1, 0, ['', ''])
+	function handleAddRows(rowIndex: number) {
+		rows.value.splice(
+			rowIndex + 1,
+			0,
+			Array.apply(null, { length: tableSize[1] }).map((_, j) => {
+				return { text: '', class: [] }
+			})
+		)
 	}
 </script>
 <template>
 	<div class="table">
-		<div class="table-rows" v-for="row in rows">
-			<div class="table-cell" v-for="cell in row" contenteditable="true">
+		<div class="table-rows" v-for="(row, rowIndex) in rows">
+			<div
+				class="table-cell"
+				v-for="(cell, cellIndex) in row"
+				@keyup="($event) => onKeyupHandler(rowIndex, cellIndex, $event)"
+				contenteditable="true"
+			>
 				{{ cell.text }}
 			</div>
-			<div class="resize-cell"></div>
+			<div class="add-rows">
+				<button @click="() => handleAddRows(rowIndex)"></button>
+			</div>
+			<!-- <button class="select-rows"></button> -->
 		</div>
 	</div>
-	<pre>
+	<!-- <pre>
 
     <code>
   {{ JSON.stringify(rows,null,4) }}      
     </code>
-  </pre>
+  </pre> -->
 </template>
 
 <style lang="less">
@@ -45,6 +54,9 @@
 		flex-direction: column;
 		width: 100%;
 		height: 100%;
+		* {
+			box-sizing: border-box;
+		}
 		.table-rows {
 			&:first-child {
 				border-top: 1px solid #f1f1f1;
@@ -55,25 +67,43 @@
 			width: 100%;
 			height: 100%;
 			position: relative;
-			.resize-x {
-				width: 100%;
-				height: 3px;
-				background: #f90;
-				position: absolute;
-			}
 			.table-cell {
 				&:first-child {
 					border-left: 1px solid #f1f1f1;
 				}
-				display: flex;
 				border-right: 1px solid #f1f1f1;
 				flex-direction: column;
 				width: 100%;
 				height: 100%;
 				padding: 7px 15px;
 				cursor: edit;
-				&:hover {
-					background: #e2e2e2;
+			}
+			.resize-x {
+				width: 100%;
+				height: 3px;
+				background: #f90;
+				position: absolute;
+			}
+			.add-rows {
+				padding: 3px;
+				position: absolute;
+				left: 0;
+				bottom: 0;
+				transform: translate(-50%, 50%);
+				z-index: 2;
+				&:hover button {
+					opacity: 1;
+				}
+				button {
+					transition: opacity 0.4s;
+					opacity: 0;
+					padding: 0;
+					width: 7px;
+					height: 7px;
+					border: none;
+					outline: none;
+					background: #f90;
+					border-radius: 50%;
 				}
 			}
 		}
